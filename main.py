14483f4b7c09bcf51970a7c6b4c5cff7b084d4bc
Chaitanya Afle <aflechaitanya@gmail.com>
2025-01-19 12:35:36 -0500

    create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from battleship_env import BattleshipEnv
+from visualization import visualize_state_ascii, visualize_state_matplotlib, play_and_visualize
+
+if __name__ == "__main__":
+    play_and_visualize()
